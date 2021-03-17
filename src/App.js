36cbec4624b2bc2f7f5fec1990f73c87df159031
Jourdan Ken Catarina <jourdancatarina3@gmail.com>
2021-03-17 12:58:21 +0800

    Fix URL

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      ``
     );
     const data = await response.json();
     setRecipes(data.hits);
